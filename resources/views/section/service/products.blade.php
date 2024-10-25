b8197736229bd46cbe511b5a0a65efb7fecd34ee
Omar Khaled <OmarKhaled102002@gmail.com>
2024-10-26 01:49:06 +0300

    change

--- resources/views/section/service/products.blade.php
+++ resources/views/section/service/products.blade.php
@@ -5,153 +5,22 @@
             <div class="cases">
              
                 <div class="cases-content wow fadeInUp slow" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
-                    <div class="row mixitup" id="MixItUp921277"><div class="col-lg-4 mix-item Architecture Landscape">
+                    <div class="row mixitup" id="MixItUp921277">
+                        @foreach ($service->products as $product)
+                        <div class="col-lg-4 mix-item Architecture Furniture">
                             <div class="case-card">
-                                <a href="{{asset('assets')}}/img/portfolio/case4.jpg" class="img" data-fancybox="cases">
-                                    <img src="{{asset('assets')}}/img/portfolio/case4.jpg" alt="" class="img-cover">
+                                <a href="" class="img" data-fancybox="cases">
+                                    <img src="{{$product->getMedia('products')->first()->getUrl()}}" alt="" class="img-cover">
                                 </a>
                                 <div class="info">
-                                    <div class="tags mb-30">
-                                        <a href="#"> Architecture </a>
-                                        <a href="#"> Landscape </a>
-                                    </div>
-                                    <h3 class="title fsz-35 mb-20"> <a href="#" class="hover-orange1">
-                                            Gordon’s Villa </a> </h3>
-                                    <div class="text color-666"> This area will is short description of
-                                        project, you can <br> select to show or hide it </div>
+                                    <h3 class="title fsz-35 mb-20"> <a href="" class="hover-orange1"> Polo
+                                            {{$product->name}} </a> </h3>
+                                    <div class="text color-666">  {{$product->details}} </div>
                                 </div>
                             </div>
-                        </div> <div class="col-lg-4 mix-item Interior">
-                            <div class="case-card">
-                                <a href="{{asset('assets')}}/img/portfolio/case5.jpg" class="img" data-fancybox="cases">
-                                    <img src="{{asset('assets')}}/img/portfolio/case5.jpg" alt="" class="img-cover">
-                                </a>
-                                <div class="info">
-                                    <div class="tags mb-30">
-                                        <a href="#"> Interior </a>
-                                    </div>
-                                    <h3 class="title fsz-35 mb-20"> <a href="#" class="hover-orange1">
-                                            Wasabi House </a> </h3>
-                                    <div class="text color-666"> This area will is short description of
-                                        project, you can <br> select to show or hide it, this a sample short
-                                        <br> paragraph for this. </div>
-                                </div>
-                            </div>
-                        </div> <div class="col-lg-4 mix-item Architecture Landscape">
-                            <div class="case-card">
-                                <a href="{{asset('assets')}}/img/portfolio/case6.jpg" class="img" data-fancybox="cases">
-                                    <img src="{{asset('assets')}}/img/portfolio/case6.jpg" alt="" class="img-cover">
-                                </a>
-                                <div class="info">
-                                    <div class="tags mb-30">
-                                        <a href="#"> Architecture </a>
-                                        <a href="#"> Landscape </a>
-                                    </div>
-                                    <h3 class="title fsz-35 mb-20"> <a href="#" class="hover-orange1">
-                                            Private Pool </a> </h3>
-                                    <div class="text color-666"> This area will is short description of
-                                        project </div>
-                                </div>
-                            </div>
-                        </div> <div class="col-lg-4 mix-item Architecture Furniture">
-                            <div class="case-card">
-                                <a href="{{asset('assets')}}/img/portfolio/case7.jpg" class="img" data-fancybox="cases">
-                                    <img src="{{asset('assets')}}/img/portfolio/case7.jpg" alt="" class="img-cover">
-                                </a>
-                                <div class="info">
-                                    <div class="tags mb-30">
-                                        <a href="#"> Architecture </a>
-                                        <a href="#"> Furniture </a>
-                                    </div>
-                                    <h3 class="title fsz-35 mb-20"> <a href="#" class="hover-orange1">
-                                            Carlo’s Villa </a> </h3>
-                                    <div class="text color-666"> This area will is short description of
-                                        project, you can <br> select to show or hide it, this a sample short
-                                        <br> paragraph for this. </div>
-                                </div>
-                            </div>
-                        </div> <div class="col-lg-4 mix-item Interior Furniture">
-                            <div class="case-card">
-                                <a href="{{asset('assets')}}/img/portfolio/case8.jpg" class="img" data-fancybox="cases">
-                                    <img src="{{asset('assets')}}/img/portfolio/case8.jpg" alt="" class="img-cover">
-                                </a>
-                                <div class="info">
-                                    <div class="tags mb-30">
-                                        <a href="#"> Interior </a>
-                                        <a href="#"> Furniture </a>
-                                    </div>
-                                    <h3 class="title fsz-35 mb-20"> <a href="#" class="hover-orange1">
-                                            Dallas Homestay <br> Rennovation </a> </h3>
-                                    <div class="text color-666"> This area will is short description of
-                                        project, you can <br> select to show or hide it, this a sample short
-                                        <br> paragraph for this. </div>
-                                </div>
-                            </div>
-                        </div> <div class="col-lg-4 mix-item Architecture Landscape">
-                            <div class="case-card">
-                                <a href="{{asset('assets')}}/img/portfolio/case9.jpg" class="img" data-fancybox="cases">
-                                    <img src="{{asset('assets')}}/img/portfolio/case9.jpg" alt="" class="img-cover">
-                                </a>
-                                <div class="info">
-                                    <div class="tags mb-30">
-                                        <a href="#"> Architecture </a>
-                                        <a href="#"> Landscape </a>
-                                    </div>
-                                    <h3 class="title fsz-35 mb-20"> <a href="#" class="hover-orange1">
-                                            Backyard Redesign </a> </h3>
-                                    <div class="text color-666"> This area will is short description of
-                                        project, you can <br> select to show or hide it </div>
-                                </div>
-                            </div>
-                        </div> <div class="col-lg-4 mix-item Architecture Landscape">
-                            <div class="case-card">
-                                <a href="{{asset('assets')}}/img/portfolio/case10.jpg" class="img" data-fancybox="cases">
-                                    <img src="{{asset('assets')}}/img/portfolio/case10.jpg" alt="" class="img-cover">
-                                </a>
-                                <div class="info">
-                                    <div class="tags mb-30">
-                                        <a href="#"> Architecture </a>
-                                        <a href="#"> Landscape </a>
-                                    </div>
-                                    <h3 class="title fsz-35 mb-20"> <a href="#" class="hover-orange1">
-                                            Container Houses </a> </h3>
-                                    <div class="text color-666"> This area will is short description of
-                                        project </div>
-                                </div>
-                            </div>
-                        </div> <div class="col-lg-4 mix-item Architecture Furniture">
-                            <div class="case-card">
-                                <a href="{{asset('assets')}}/img/portfolio/case11.jpg" class="img" data-fancybox="cases">
-                                    <img src="{{asset('assets')}}/img/portfolio/case11.jpg" alt="" class="img-cover">
-                                </a>
-                                <div class="info">
-                                    <div class="tags mb-30">
-                                        <a href="#"> Architecture </a>
-                                        <a href="#"> Furniture </a>
-                                    </div>
-                                    <h3 class="title fsz-35 mb-20"> <a href="#" class="hover-orange1">
-                                            Luthor’s Estate </a> </h3>
-                                    <div class="text color-666"> This area will is short description of
-                                        project, you can <br> select to show or hide it </div>
-                                </div>
-                            </div>
-                        </div> <div class="col-lg-4 mix-item Architecture Furniture">
-                            <div class="case-card">
-                                <a href="{{asset('assets')}}/img/portfolio/case12.jpg" class="img" data-fancybox="cases">
-                                    <img src="{{asset('assets')}}/img/portfolio/case12.jpg" alt="" class="img-cover">
-                                </a>
-                                <div class="info">
-                                    <div class="tags mb-30">
-                                        <a href="#"> Architecture </a>
-                                        <a href="#"> Furniture </a>
-                                    </div>
-                                    <h3 class="title fsz-35 mb-20"> <a href="#" class="hover-orange1"> Polo
-                                            C25 Duplex </a> </h3>
-                                    <div class="text color-666"> This area will is short description of
-                                        project, you can <br> select to show or hide it </div>
-                                </div>
-                            </div>
-                        </div></div>
+                        </div>
+                        @endforeach
+                    </div>
                  
                 </div>
             </div>
