4d2f155acfa71834c2d29b932b040a47165e4de7
Omar Khaled <OmarKhaled102002@gmail.com>
2024-10-26 00:19:32 +0300

    change

--- resources/views/main/scripts.blade.php
+++ resources/views/main/scripts.blade.php
@@ -5,18 +5,18 @@
   <script src="{{asset('assets/js/lib/jquery-3.0.0.min.js')}}"></script>
   <script src="{{asset('assets/js/lib/jquery-migrate-3.0.0.min.js')}}"></script>
   <script src="{{asset('assets/js/lib/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{asset('assets/js/lib/wow.min.js')}}"></script>
-  <script src="{{asset('assets/js/lib/jquery.fancybox.js')}}"></script>
+  {{-- <script src="{{asset('assets/js/lib/wow.min.js')}}"></script> --}}
+  {{-- <script src="{{asset('assets/js/lib/jquery.fancybox.js')}}"></script>
   <script src="{{asset('assets/js/lib/lity.js')}}"></script>
   <script src="{{asset('assets/js/lib/swiper8-bundle.min.js')}}"></script>
   <script src="{{asset('assets/js/lib/jquery.waypoints.min.js')}}"></script>
   <script src="{{asset('assets/js/lib/jquery.counterup.js')}}"></script>
-  <script src="{{asset('assets/js/lib/parallaxie.js')}}"></script>
+  <script src="{{asset('assets/js/lib/parallaxie.js')}}"></script> --}}
   <!-- === Gsap === -->
-  <script src="{{asset('assets/js/gsap_lib/gsap.min.js')}}"></script>
-  <script src="{{asset('assets/js/gsap_lib/ScrollSmoother.min.js')}}"></script>
-  <script src="{{asset('assets/js/gsap_lib/ScrollTrigger.min.js')}}"></script>
-  <script src="{{asset('assets/js/gsap_lib/SplitText.min.js')}}"></script>
+  {{-- <script src="{{asset('assets/js/gsap_lib/gsap.min.js')}}"></script> --}}
+  {{-- <script src="{{asset('assets/js/gsap_lib/ScrollSmoother.min.js')}}"></script> --}}
+  {{-- <script src="{{asset('assets/js/gsap_lib/ScrollTrigger.min.js')}}"></script> --}}
+  {{-- <script src="{{asset('assets/js/gsap_lib/SplitText.min.js')}}"></script> --}}
   <!-- === common === -->
   <script src="{{asset('assets/js/common_js.js')}}"></script>
 
