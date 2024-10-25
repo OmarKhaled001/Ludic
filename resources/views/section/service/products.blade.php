2900908cf19ead5c32ffdca27149c03b6d4403c5
Omar Khaled <OmarKhaled102002@gmail.com>
2024-10-25 23:56:44 +0300

    change

--- resources/views/section/service/products.blade.php
+++ resources/views/section/service/products.blade.php
@@ -1,7 +1,7 @@
 <section class="tc-latest-cases-style1">
     <div class="container">
         <div class="content">
-            <h3 class="fsz-45 text-capitalize mb-90 js-splittext-lines" style="perspective: 400px;"><div style="display: block; text-align: start; position: relative; translate: none; rotate: none; scale: none; transform-origin: 620px 27px; transform: translate3d(0px, 0px, 0px); opacity: 1;"> Latest Cases </div></h3>
+            <h3 class="fsz-45 text-capitalize mb-90 js-splittext-lines" style="perspective: 400px;"><div style="display: block; text-align: center; position: relative; translate: none; rotate: none; scale: none; transform-origin: 620px 27px; transform: translate3d(0px, 0px, 0px); opacity: 1;"> Products </div></h3>
             <div class="cases">
              
                 <div class="cases-content wow fadeInUp slow" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
