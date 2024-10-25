ca95e0170a2fc00dc21f2c9c7e22ef57f2f8523b
Omar Khaled <OmarKhaled102002@gmail.com>
2024-10-25 23:23:38 +0300

    change

--- public/assets/js/common_js.js
+++ public/assets/js/common_js.js
@@ -1,113 +1,99 @@
-$( function() {
+$(function () {
 
     var wind = $(window);
 
-    wow = new WOW({
-        boxClass: 'wow',
-        animateClass: 'animated',
-        offset: 200,
-        mobile: false,
-        live: false
-    });
-    wow.init();
+    // WOW.js initialization for large screens
+    if (wind.width() > 768) {
+        var wow = new WOW({
+            boxClass: 'wow',
+            animateClass: 'animated',
+            offset: 200,
+            mobile: false,
+            live: false
+        });
+        wow.init();
+    }
 
-    // ---------- background change -----------
+    // Background change based on data attribute
     var pageSection = $(".bg-img");
-    pageSection.each(function (indx) {
-
+    pageSection.each(function () {
         if ($(this).attr("data-background")) {
             $(this).css("background-image", "url(" + $(this).data("background") + ")");
         }
     });
-    
 
-    // ---------- to top -----------
-
-    wind.on("scroll", function() {
-
+    // Scroll to top button functionality
+    wind.on("scroll", function () {
         var bodyScroll = wind.scrollTop(),
-            toTop = $("#to_top")
+            toTop = $("#to_top");
 
         if (bodyScroll > 700) {
-
             toTop.addClass("show");
-
         } else {
-
             toTop.removeClass("show");
         }
     });
 
-    $('#to_top').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 0);
+    $('#to_top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 600); // Smooth scroll duration set to 600ms
         return false;
     });
 
-    // -------- counter --------
+    // Counter animation
     $('.counter').countUp({
         delay: 10,
         time: 2000
     });
 
-
-     /* ==  float_box_container button  == */
-    $( ".float_box_container" ).mousemove(function(e) {
-        var parentOffset = $(this).offset(); 
+    // Floating box effect in a container
+    $(".float_box_container").mousemove(function (e) {
+        var parentOffset = $(this).offset();
         var relX = e.pageX - parentOffset.left;
         var relY = e.pageY - parentOffset.top;
-        $(".float_box").css({"left": relX, "top": relY });
+        $(".float_box").css({ "left": relX, "top": relY });
         $(".float_box").addClass("show");
     });
-    $( ".float_box_container" ).mouseleave(function(e) {
+
+    $(".float_box_container").mouseleave(function () {
         $(".float_box").removeClass("show");
     });
 
-    // -------- fav-btn --------
-    $(".fav-btn").on("click", function(){
+    // Favorite button toggle
+    $(".fav-btn").on("click", function () {
         $(this).toggleClass("active");
-    })
+    });
 
-    // -------- cls --------
-    $(".cls").on("click", function(){
+    // Close button functionality
+    $(".cls").on("click", function () {
         $(this).parent().fadeOut();
-    })
+    });
 
-    // ------------ working in desktop -----------
-    if ($(window).width() > 991) {
-        $('.parallaxie').parallaxie({
-        });
+    // Parallax effect for larger screens
+    if (wind.width() > 991) {
+        $('.parallaxie').parallaxie();
     }
 
-    // ---------- tooltip -----------
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    // Tooltip initialization
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
 
-    // ---------- to top -----------
-    wind.on("scroll", function() {
-
+    // Scroll to top (repeated code for class-based to_top button)
+    wind.on("scroll", function () {
         var bodyScroll = wind.scrollTop(),
-            toTop = $(".to_top")
+            toTop = $(".to_top");
 
         if (bodyScroll > 700) {
-
             toTop.addClass("show");
-
         } else {
-
             toTop.removeClass("show");
         }
     });
-        
 });
 
-
-
-// ------------ Preloader -----------
-$( function() {
+// Preloader animation with GSAP
+$(function () {
     const svg = document.getElementById("svg");
     const tl = gsap.timeline();
     const curve = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
@@ -116,54 +102,63 @@ $( function() {
     tl.to(".loader-wrap-heading .load-text , .loader-wrap-heading .cont , .loader-wrap-heading .logo", {
         delay: 1.5,
         y: -100,
-        opacity: 0,
+        opacity: 0
     });
     tl.to(svg, {
         duration: 0.5,
         attr: { d: curve },
-        ease: "power2.easeIn",
+        ease: "power2.easeIn"
     }).to(svg, {
         duration: 0.5,
         attr: { d: flat },
-        ease: "power2.easeOut",
+        ease: "power2.easeOut"
     });
     tl.to(".loader-wrap", {
-        y: -1500,
+        y: -1500
     });
     tl.to(".loader-wrap", {
         zIndex: -1,
-        display: "none",
+        display: "none"
     });
-    tl.from(
-        "header , .header",
-        {
-            y: 200,
-        },
-        "-=1.5"
-    );
-    tl.from(
-        "header .container , .header .container",
-        {
-            y: 40,
-            opacity: 0,
-            delay: 0.3,
-        },
-        "-=1.5"
-    );
+    tl.from("header , .header", {
+        y: 200
+    }, "-=1.5");
+    tl.from("header .container , .header .container", {
+        y: 40,
+        opacity: 0,
+        delay: 0.3
+    }, "-=1.5");
 });
 
-
+// Preloader end on window load
 $(window).on("load", function () {
-
-    // ------------ Preloader -----------
     var body = $('body');
     body.addClass('loaded');
     setTimeout(function () {
         body.removeClass('loaded');
     }, 1500);
-
 });
 
+// Media queries and responsiveness
+$(function () {
+    // Adjust font sizes and paddings based on screen size
+    function adjustForDevice() {
+        if (wind.width() < 768) {
+            // Mobile devices
+            $('h1, h2, h3, p').css('font-size', '14px');
+            $('.bg-img').css('background-size', 'cover');
+        } else if (wind.width() >= 768 && wind.width() <= 991) {
+            // Tablet devices
+            $('h1, h2, h3, p').css('font-size', '16px');
+        } else {
+            // Larger screens
+            $('h1, h2, h3, p').css('font-size', '18px');
+        }
+    }
 
-
-
+    // Run the function on page load and resize
+    adjustForDevice();
+    wind.resize(function () {
+        adjustForDevice();
+    });
+});
