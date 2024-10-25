52a33ce2cee47d1ae6deea0dacbd5463e366f685
Omar Khaled <OmarKhaled102002@gmail.com>
2024-10-25 23:08:57 +0300

    change

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',             [PageController::class,'index'])        ->name('index');
 Route::get('/about',        [PageController::class,'about'])        ->name('about');
-Route::get('/service/slug', [PageController::class,'service'])      ->name('service');
+Route::get('/service/{slug}', [PageController::class,'service'])      ->name('service');
 Route::get('/contact',      [PageController::class,'contact'])      ->name('contact');
 Route::post('/contact',     [PageController::class,'contactForm'])  ->name('contact');
 
