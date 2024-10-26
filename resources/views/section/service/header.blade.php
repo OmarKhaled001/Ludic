a83c90782cf92b8623aeab0893d2bd9195841363
Omar Khaled <OmarKhaled102002@gmail.com>
2024-10-27 01:59:48 +0300

    change

--- resources/views/section/service/header.blade.php
+++ resources/views/section/service/header.blade.php
@@ -1,4 +1,4 @@
-<header class="col-10">
+<header class="container" style="translate: none; rotate: none; scale: none; transform: translate(0px, 0px);">
     <div class="container" style="translate: none; rotate: none; scale: none; opacity: 1; transform: translate(0px, 0px);">
         <div class="info">
             <h1 class="title"> {{$service->name}} </h1>
