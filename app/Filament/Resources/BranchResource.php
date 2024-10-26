25489c5b101ac0c7c134e1ce22ab93da9e292a81
Omar Khaled <OmarKhaled102002@gmail.com>
2024-10-26 18:56:38 +0300

    change

--- app/Filament/Resources/BranchResource.php
+++ app/Filament/Resources/BranchResource.php
@@ -2,19 +2,16 @@
 
 namespace App\Filament\Resources;
 
-use Filament\Forms;
-use Filament\Tables;
-use App\Models\Branch;
-use Filament\Forms\Form;
-use Filament\Tables\Table;
-use Filament\Resources\Resource;
-use Illuminate\Database\Eloquent\Builder;
-use Cheesegrits\FilamentGoogleMaps\Forms\Components\GoogleMaps;
-use Cheesegrits\FilamentGoogleMaps\Fields\Map;
-
 use App\Filament\Resources\BranchResource\Pages;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\BranchResource\RelationManagers;
+use App\Models\Branch;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class BranchResource extends Resource
 {
@@ -32,7 +29,12 @@ class BranchResource extends Resource
             Forms\Components\TextInput::make('address')
                 ->required()
                 ->label('Branch Address'),
-                Map::make('location') ,  
+            Forms\Components\TextInput::make('latitude')
+                ->required()
+                ->label('Latitude'),
+            Forms\Components\TextInput::make('longitude')
+                ->required()
+                ->label('Longitude'),
             Forms\Components\Select::make('country_id')
                 ->relationship('country', 'name')
                 ->required()
