2af3082d64300eb4f7a9fa4f2e3e81464dc2d108
Omar Khaled <OmarKhaled102002@gmail.com>
2024-11-03 02:18:09 +0200

    change

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use App\Models\Service;
 use Illuminate\Support\ServiceProvider;
+use App\Models\Service;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,9 +22,9 @@ class AppServiceProvider extends ServiceProvider
     {
         $services = Service::all();
 
-        view()->share(
+        view()->share([
             'services', $services,
 
-        );
+        ]);
     }
 }
