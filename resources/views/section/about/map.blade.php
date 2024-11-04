5f248dce2eff1b84700b27a898fc4b2f1a000ec7
Omar Khaled <omarkhaled102002@gmail.com>
2024-11-05 01:49:58 +0200

    add

--- resources/views/section/about/map.blade.php
+++ resources/views/section/about/map.blade.php
@@ -1,4 +1,4 @@
-<section class="tc-map-style1 p-20">
+<section class="tc-map-style1 p-30 mt-20">
     <div class="container">
         <div class="title mb-20 text-center">
             <h2 class="fsz-45 "> Our Branches </h2>
