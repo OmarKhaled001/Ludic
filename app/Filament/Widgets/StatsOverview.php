5d689ad62e0e7aae2d78473b003b8aba23e13f7d
Omar Khaled <omarkhaled102002@gmail.com>
2024-11-04 23:16:54 +0200

    add

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -11,7 +11,10 @@ class StatsOverview extends BaseWidget
     {
         return [
             Stat::make('Unique views', '192.1k')
-                ->description('32k increase')
+            ->description('32k increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-up')
+            ->chart([7, 2, 10, 3, 15, 4, 17])
+            ->color('success')
                 ->descriptionIcon('heroicon-m-arrow-trending-up'),
             Stat::make('Bounce rate', '21%')
                 ->description('7% decrease')
