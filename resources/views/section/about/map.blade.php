37a40a4392439e43c663df01094f44c93675804f
Omar Khaled <omarkhaled102002@gmail.com>
2024-11-05 01:45:05 +0200

    add

--- resources/views/section/about/map.blade.php
+++ resources/views/section/about/map.blade.php
@@ -6,7 +6,7 @@
         <div class="row justify-content-between">
            
             <div class="tc-map-style1">
-                <div class="map-card wow zoomIn slow" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: zoomIn;">
+                <div class="map-card wow zoomIn slow" data-wow-delay="0.2s">
                     <div id="map"></div>
                 </div>
             </div>
