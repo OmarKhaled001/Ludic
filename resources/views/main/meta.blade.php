81989c7d16fbacd53512dbe450d4c239361f49bd
Omar Khaled <omarkhaled102002@gmail.com>
2024-11-04 17:45:30 +0200

    add

--- resources/views/main/meta.blade.php
+++ resources/views/main/meta.blade.php
@@ -7,8 +7,6 @@
 
     <!-- font family -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&display=swap" rel="stylesheet">
 
     <!-- fontawesome icons  -->
     <link rel="stylesheet" href="assets/css/lib/all.min.css">
