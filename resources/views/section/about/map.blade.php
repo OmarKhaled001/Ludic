c5bac417ff66e543a8c97099f0167d4ce2df8d23
Omar Khaled <omarkhaled102002@gmail.com>
2024-11-05 01:47:20 +0200

    add

--- resources/views/section/about/map.blade.php
+++ resources/views/section/about/map.blade.php
@@ -3,9 +3,9 @@
         <div class="title mb-20 text-center">
             <h2 class="fsz-45 "> Our Branches </h2>
         </div>
-        <div class="row justify-content-between tc-map-style1">
-           
-                    <div id="map"></div>
+        <div class="row justify-content-between">
+            <div id="map" style="height: 500px;"></div>
+
         </div>
 
     </div>
