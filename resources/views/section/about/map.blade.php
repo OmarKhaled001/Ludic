a64449bf25438f0028375b7f5dcb219a52355f3f
Omar Khaled <omarkhaled102002@gmail.com>
2024-11-05 01:40:07 +0200

    add

--- resources/views/section/about/map.blade.php
+++ resources/views/section/about/map.blade.php
@@ -1,13 +1,6 @@
-<section class="tc-team-style1">
-    <div class="container">
-        <div class="title mb-20 text-center">
-            <h2 class="fsz-45 "> Our Branches </h2>
-        </div>
-        <div class="row justify-content-between">
-            <div id="map" style="height: 500px;"></div>
 
-        </div>
-
+<section class="tc-map-style1">
+    <div class="map-card wow zoomIn slow" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: zoomIn;">
+        <div id="map"></div>
     </div>
-
 </section>
