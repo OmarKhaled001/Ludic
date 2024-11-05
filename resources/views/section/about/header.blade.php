7a325b65f35a0458f1597c4d322bd9921c91fa5f
Omar Khaled <omarkhaled102002@gmail.com>
2024-11-05 02:30:21 +0200

    add

--- resources/views/section/about/header.blade.php
+++ resources/views/section/about/header.blade.php
@@ -1,93 +1,95 @@
 
-<section class="tc-process-style2">
-    <div class="container">
-        <h2 class="fsz-45 fw-500 mb-80 text-center"> About Us</h2>
-        <div class="content">
-            <div class="row">
-                <div class="col-lg-5">
-                    <div class="accordion-side wow fadeInUp slow"
-                        style="visibility: visible; animation-name: fadeInUp;">
-                        <div class="accordion" id="accordionProcess">
-                            <div class="accordion-item">
-                                <div class="accordion-header" id="headingOne">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#collapseOne">
-                                        <span class="num"> 1 / </span>
-                                        <h3> Who We Are ? </h3>
-                                    </button>
-                                </div>
-                                <div id="collapseOne" class="accordion-collapse collapse"
-                                    data-bs-parent="#accordionProcess">
-                                    <div class="accordion-body">
-                                        <div class="text">
-                                            From the beginning, our goal has extended beyond sales and profits. We are committed to ongoing
-                                            development driven by specialized research, allowing us to offer the highest quality products,
-                                            competitive pricing, and reliable, timely supply. ludic's dedication to cultural responsibility
-                                            continues to evolve, as we share our expertise by providing technical information, training, and
-                                            supporting clients in growing their businesses both vertically and horizontally.
+<section class="about">
+    <div class="tc-process-style2">
+        <div class="container">
+            <h2 class="fsz-45 fw-500 mb-80 text-center"> About Us</h2>
+            <div class="content">
+                <div class="row">
+                    <div class="col-lg-5">
+                        <div class="accordion-side wow fadeInUp slow"
+                            style="visibility: visible; animation-name: fadeInUp;">
+                            <div class="accordion" id="accordionProcess">
+                                <div class="accordion-item">
+                                    <div class="accordion-header" id="headingOne">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                            data-bs-target="#collapseOne">
+                                            <span class="num"> 1 / </span>
+                                            <h3> Who We Are ? </h3>
+                                        </button>
+                                    </div>
+                                    <div id="collapseOne" class="accordion-collapse collapse"
+                                        data-bs-parent="#accordionProcess">
+                                        <div class="accordion-body">
+                                            <div class="text">
+                                                From the beginning, our goal has extended beyond sales and profits. We are committed to ongoing
+                                                development driven by specialized research, allowing us to offer the highest quality products,
+                                                competitive pricing, and reliable, timely supply. ludic's dedication to cultural responsibility
+                                                continues to evolve, as we share our expertise by providing technical information, training, and
+                                                supporting clients in growing their businesses both vertically and horizontally.
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="accordion-item">
-                                <div class="accordion-header" id="headingTwo">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#collapseTwo" aria-expanded="false">
-                                        <span class="num"> 2 / </span>
-                                        <h3>Our Vision </h3>
-                                    </button>
-                                </div>
-                                <div id="collapseTwo" class="accordion-collapse collapse"
-                                    data-bs-parent="#accordionProcess" style="">
-                                    <div class="accordion-body">
-                                        <div class="text">
-                                            Providing food products commensurating with our customers markets in terms
-                                            of quality prices and timely access through establishing an eVicient and
-                                            integrated import and export process in all aspects. Starting from knowing
-                                            the customers
-                                            demand, moving through each stageof the shipping process, down to after sale
-                                            services.
+                                <div class="accordion-item">
+                                    <div class="accordion-header" id="headingTwo">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                            data-bs-target="#collapseTwo" aria-expanded="false">
+                                            <span class="num"> 2 / </span>
+                                            <h3>Our Vision </h3>
+                                        </button>
+                                    </div>
+                                    <div id="collapseTwo" class="accordion-collapse collapse"
+                                        data-bs-parent="#accordionProcess" style="">
+                                        <div class="accordion-body">
+                                            <div class="text">
+                                                Providing food products commensurating with our customers markets in terms
+                                                of quality prices and timely access through establishing an eVicient and
+                                                integrated import and export process in all aspects. Starting from knowing
+                                                the customers
+                                                demand, moving through each stageof the shipping process, down to after sale
+                                                services.
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="accordion-item">
-                                <div class="accordion-header" id="headingThree">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                        data-bs-target="#collapseThree">
-                                        <span class="num"> 3 / </span>
-                                        <h3> Our Mission  </h3>
-                                    </button>
-                                </div>
-                                <div id="collapseThree" class="accordion-collapse collapse"
-                                    data-bs-parent="#accordionProcess">
-                                    <div class="accordion-body">
-                                        <div class="text">
-                                            Our mission is to guarantee the growth and success of our clients by
-                                            providing exceptional soutions and unwavering su pport
-
+                                <div class="accordion-item">
+                                    <div class="accordion-header" id="headingThree">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                            data-bs-target="#collapseThree">
+                                            <span class="num"> 3 / </span>
+                                            <h3> Our Mission  </h3>
+                                        </button>
+                                    </div>
+                                    <div id="collapseThree" class="accordion-collapse collapse"
+                                        data-bs-parent="#accordionProcess">
+                                        <div class="accordion-body">
+                                            <div class="text">
+                                                Our mission is to guarantee the growth and success of our clients by
+                                                providing exceptional soutions and unwavering su pport
+    
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+                              
                             </div>
-                          
+                           
                         </div>
-                       
                     </div>
-                </div>
-                <div class="col-lg-7">
-                    <div class="imgs">
-                        <div class="img" data-lag="0.2" data-speed="1"
-                            style="translate: none; rotate: none; scale: none; transform: translate(50px, 0px); will-change: transform;">
-                            <img src="{{ asset('assets/img/exp.png') }}" alt="" class="img-cover">
+                    <div class="col-lg-7">
+                        <div class="imgs">
+                            <div class="img" data-lag="0.2" data-speed="1"
+                                style="translate: none; rotate: none; scale: none; transform: translate(50px, 0px); will-change: transform;">
+                                <img src="{{ asset('assets/img/exp.png') }}" alt="" class="img-cover">
+                            </div>
+                        
+                         
                         </div>
-                    
-                     
                     </div>
                 </div>
             </div>
         </div>
+        <img src="{{ asset('assets') }}/img/prc_bg.png" alt="" class="bg" data-speed="1.2" data-lag="0" style="translate: none; rotate: none; scale: none; transform: translate(0px, 194.75px); will-change: transform;">
+    
     </div>
-    <img src="{{ asset('assets') }}/img/prc_bg.png" alt="" class="bg" data-speed="1.2" data-lag="0"
-        style="translate: none; rotate: none; scale: none; transform: translate(0px, 194.75px); will-change: transform;">
 </section>
