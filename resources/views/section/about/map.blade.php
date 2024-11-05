c96e90cd7cab6751491c64dc6adbadd6874acdb1
Omar Khaled <omarkhaled102002@gmail.com>
2024-11-05 02:01:01 +0200

    add

--- resources/views/section/about/map.blade.php
+++ resources/views/section/about/map.blade.php
@@ -3,7 +3,7 @@
         <div class="title mb-20 text-center">
             <h2 class="fsz-45 "> Our Branches </h2>
         </div>
-        <div class="row justify-content-between map-card">
+        <div class="row justify-content-between map-card"  data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: zoomIn;">
             <div id="map"></div>
 
         </div>
