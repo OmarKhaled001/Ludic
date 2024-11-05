1e3b2ee6c6ef3fba3c6c3b3d98d9fbffe1c085c6
Omar Khaled <omarkhaled102002@gmail.com>
2024-11-05 14:37:06 +0200

    0

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -12,14 +12,14 @@
         <title>Ludic - {{$service->name}}</title>
         @include('main.meta')
 </head>
-<body>
+<body style="height: 6351px; touch-action: pan-x pinch-zoom; overscroll-behavior: none; scroll-behavior: auto;">
     @include('main.loader')
-    <div class="smooth-scroll-content" id="scrollsmoother-container">
+    <div class="smooth-scroll-content" id="scrollsmoother-container" style="inset: 0px; width: 100%; height: 100%; position: fixed; overflow: hidden;">
         @include('main.nav')
         @include('section.service.header')
         <main>
-            @include('section.service.info')
-            @include('section.service.products')
+            {{-- @include('section.service.info')
+            @include('section.service.products') --}}
             @include('section.home.mail')
             
         </main>
