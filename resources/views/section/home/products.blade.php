b917eebc2c0d7d48811bbbb512fa995463e9acdb
Omar Khaled <omarkhaled102002@gmail.com>
2024-11-06 15:36:55 +0200

    add

--- resources/views/section/home/products.blade.php
+++ resources/views/section/home/products.blade.php
@@ -1,75 +1,20 @@
 <section class="tc-team-style1">
     <div class="container">
-        <div class="mb-80 js-splittext-lines text-center" style="perspective: 400px;"><div style="display: block; text-align: start; position: relative; translate: none; rotate: none; scale: none; transform-origin: 620px 27px; transform: translate3d(0px, 0px, 0px); opacity: 1;"><h2 class="fsz-45">Best Products</h2></div></div>
+        <div class="mb-80 js-splittext-lines text-center" style="perspective: 400px;"><div style="display: block; text-align: start; position: relative; translate: none; rotate: none; scale: none; transform-origin: 620px 27px; transform: translate3d(0px, 0px, 0px); opacity: 1;"><h2 class="fsz-45">Products</h2></div></div>
         <div class="members">
             <div class="row">
-                <div class="col-lg-2">
+                @foreach ($services as $service)
+                <div class="col-lg-4">
                     <div class="member-colmn">
                         <a href="#" class="team-card wow zoomIn" style="visibility: visible; animation-name: zoomIn;">
                             <div class="img" style="height: 181.656px;">
-                                <img src="{{asset('assets')}}/img/team/team1.jpg" alt="" class="img-cover">
+                                <img src="{{$service->getMedia('services')->first()->getUrl()}}" alt="{{$service->name}}" class="img-cover">
                             </div>
-                            <h5 class="title"> Ethiopian <br> Yirgacheffe  </h5>
-                        </a>
-                        <a href="#" class="team-card wow zoomIn" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: zoomIn;">
-                            <div class="img" style="height: 181.656px;">
-                                <img src="{{asset('assets')}}/img/team/team2.jpg" alt="" class="img-cover">
-                            </div>
-                            <h5 class="title"> Medjool <br> Dates </h5>
+                            <h5 class="title">{{$service->name}}</h5>
                         </a>
                     </div>
                 </div>
-                <div class="col-lg-4">
-                    <div class="member-colmn">
-                        <a href="#" class="team-card wow zoomIn" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: zoomIn;">
-                            <div class="img" style="height: 393.323px;">
-                                <img src="{{asset('assets')}}/img/team/team3.jpg" alt="" class="img-cover">
-                            </div>
-                            <h5 class="title"> Pistachios </h5>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-2">
-                    <div class="member-colmn">
-                        <a href="#" class="team-card wow zoomIn" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: zoomIn;">
-                            <div class="img" style="height: 181.656px;">
-                                <img src="{{asset('assets')}}/img/team/team4.jpg" alt="" class="img-cover">
-                            </div>
-                            <h5 class="title"> jhon <br> handerson </h5>
-                        </a>
-                        <a href="#" class="team-card wow zoomIn" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: zoomIn;">
-                            <div class="img" style="height: 181.656px;">
-                                <img src="{{asset('assets')}}/img/team/team5.jpg" alt="" class="img-cover">
-                            </div>
-                            <h5 class="title"> jhon <br> handerson </h5>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-2 order-last order-lg-0">
-                    <div class="member-colmn">
-                        <a href="#" class="team-card wow zoomIn" data-wow-delay="0.5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">
-                            <div class="img" style="height: 181.656px;">
-                                <img src="{{asset('assets')}}/img/team/team6.jpg" alt="" class="img-cover">
-                            </div>
-                            <h5 class="title"> Roasted <br> Almonds </h5>
-                        </a>
-                        <a href="#" class="team-card team-text wow zoomIn" data-wow-delay="0.6s" style="visibility: visible; animation-delay: 0.6s; animation-name: zoomIn;">
-                            <div class="img" style="height: 181.656px;">
-                                <h5 class="title"> <div class="con"> <span> See All Our <br> Products </span> <span class="arrow"> <i class="ti-arrow-top-right"></i> </span> </div> </h5>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-2">
-                    <div class="member-colmn">
-                        <a href="#" class="team-card wow zoomIn" data-wow-delay="0.7s" style="visibility: visible; animation-delay: 0.7s; animation-name: zoomIn;">
-                            <div class="img" style="height: 181.656px;">
-                                <img src="{{asset('assets')}}/img/team/team7.jpg" alt="" class="img-cover">
-                            </div>
-                            <h5 class="title"> jhon <br> handerson </h5>
-                        </a>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
         
